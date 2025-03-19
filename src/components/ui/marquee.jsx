9712b0791a78699519147bf02ad136b2b2242da3
vieerr <olivier_paspuel@hotmail.com>
2025-03-19 12:27:20 -0500

    pfp and useful links added

--- src/components/ui/marquee.jsx
+++ src/components/ui/marquee.jsx
@@ -1,7 +1,7 @@
 export default function Marquee({ items }) {
   return (
     <div className="relative flex md:w-2/3 mx-auto overflow-x-hidden ">
-      <div className="animate-marquee whitespace-nowrap py-6">
+      <div className="animate-marquee whitespace-nowrap py-4">
         {items.map((item) => {
           return (
             <span key={item} className="mx-4 text-4xl">
@@ -10,7 +10,7 @@ export default function Marquee({ items }) {
           );
         })}
       </div>
-      <div className="absolute top-0 animate-marquee2 whitespace-nowrap py-6">
+      <div className="absolute top-0 animate-marquee2 whitespace-nowrap py-4">
         {items.map((item) => {
           return (
             <span key={item} className="mx-4 text-4xl">
